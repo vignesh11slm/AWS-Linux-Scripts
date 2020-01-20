adb16b3300127bef16ffc521ba8acc1d8e91a767
vignesh11slm <vignesh11slm@hotmail.com>
2020-01-20 10:55:56 +0530

    Update Cloud9_bootstrap.sh

--- Cloud9_bootstrap.sh
+++ Cloud9_bootstrap.sh
@@ -2,3 +2,13 @@
 sudo yum update -y 
 sudo pip install --upgrade pip
 sudo pip install pipenv boto3 c7n ipython
+
+
+aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
+
+aws ec2 describe-vpcs --filters "Name=tag:Name,Values=myhostname"
+aws ec2 describe-vpcs --vpc-ids vpc-06e4ab6c6cEXAMPLE
+
+aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
+
+
