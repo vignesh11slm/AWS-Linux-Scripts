325b6d2aadcc9f04de485c9babf3563167288798
vignesh11slm <vignesh11slm@hotmail.com>
2020-01-20 13:16:17 +0530

    Update Cloud9_bootstrap.sh

--- Cloud9_bootstrap.sh
+++ Cloud9_bootstrap.sh
@@ -25,3 +25,4 @@ SUBNETID="$(aws ec2 describe-subnets --filter "Name=vpc-id,Values=$DEFAULTVPCID"
 
 aws ec2 describe-security-groups --filter "Name=vpc-id,Values=$DEFAULTVPCID" "Name=group-name,Values=default" --query "SecurityGroups[0].GroupId" --output text
 
+aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????.?-x86_64-gp2' 'Name=state,Values=available' --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' --output text
